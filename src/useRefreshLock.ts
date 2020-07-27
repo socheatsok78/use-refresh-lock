80499878f14c86fe7e58dc01d7abbc7da725d0be
Socheat Sok <alex@socheat.net>
2020-07-28 00:10:08 +0700

    fix(core): refactor 'useRefreshLock' callback, execute only when unload

--- src/useRefreshLock.ts
+++ src/useRefreshLock.ts
@@ -6,11 +6,12 @@ import { useEventListener, RemoveEventListenerCallback } from "./utils/useEvent"
  * Ensure that user allow the browser to refresh
  */
 export function useRefreshLock(callback: () => void = () => {}, message: string = ''): RemoveEventListenerCallback {
+    if (typeof callback === 'function') {
+        useEventListener('unload', callback)
+    }
+
     const release = useEventListener('beforeunload', function (event) {
         event.preventDefault();
-
-        if (typeof callback === 'function') callback();
-
         event.returnValue = message;
         return event.returnValue;
     }, { capture: true })
