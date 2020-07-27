88d5a0a683b92729b11e279cecddf5d631dbfa92
Socheat Sok <alex@socheat.net>
2020-07-27 23:41:56 +0700

    fix(core): update 'useRefreshLock' callback execution

--- src/useRefreshLock.ts
+++ src/useRefreshLock.ts
@@ -5,15 +5,15 @@ import { useEventListener } from "./utils/useEvent";
  *
  * Ensure that user allow the browser to refresh
  */
-export function useRefreshLock(message: string = '', callback: () => void) {
-    if (typeof callback === 'function') {
-        useEventListener('unload', callback)
-    }
-
+export function useRefreshLock(callback: () => void, message: string = '') {
     const release = useEventListener('beforeunload', function (event) {
         event.preventDefault();
+
+        if (typeof callback === 'function') callback();
+
         event.returnValue = message;
-    })
+        return event.returnValue;
+    }, { capture: true })
 
     return [ release ];
 }
